4f1c5631d35c16443a74496b0e32211ed7669637
Bruce Ravel <bravel@bnl.gov>
2017-04-08 08:50:04 -0400

    more poking at CW

--- make_game.sh
+++ make_game.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
 org2latex rules.org
+pdflatex rules.tex
 pdflatex book.tex
-inkscape -A 0words.pdf 0words.svg
-inkscape -A 1word.pdf 1word.svg
-inkscape -A 2words.pdf 2words.svg
+
+files='0words 1word 2words remove'
+for f in $files; do
+    inkscape -A $f.pdf $f.svg
+done
+
+pdfunite 0words.pdf 1word.pdf 1word.pdf 2words.pdf remove.pdf all_rules.pdf
